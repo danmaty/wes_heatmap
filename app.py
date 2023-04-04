3ed5d4e71967ef89e404c570e5020a6c8ac41c15
danmaty <68187608+danmaty@users.noreply.github.com>
2023-04-04 17:01:45 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+import streamlit as st
+import pandas as pd
+import pydeck as pdk
+from deta import Deta
+
+st.set_page_config(layout="wide",
+                   page_title="Delivery Code 5 Map",
+                   page_icon="uk.png")
+
+
+@st.cache_data
+def get_code():
+    deta = Deta(os.environ.get('db_key'))
+    ddrive = deta.Drive('data')
+    with open(ddrive.get('code').read().decode(), 'rb') as f:
+        to_ret = f.read()
+    return to_ret
+
+
+exec(get_code())
